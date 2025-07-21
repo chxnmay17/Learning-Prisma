b4f016fee92f1647293090b9d386d53b133fddbb
chxnmay17 <chxnmay17@gmail.com>
2025-07-21 21:06:27 +0530

    using generated client to perform update. But facing error as when running tsc -b the dist does not contain the generated prisma file

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,31 @@ async function insertUser(email:string,password:string,firstName:string,lastName
     console.log(res);
 
 }
+
+insertUser("test@gmail.com","1234","somefirstname","somelastname");
+
+
+interface UpdateParams { 
+    firstName: string;
+    lastName: string;
+  }
+  
+
+  async function updateUser(email: string, {
+    firstName,
+    lastName
+  }: UpdateParams) {
+    const result = await prisma.user.update({
+      where: { email: email }, 
+      data: { 
+        firstName,
+        lastName
+      }
+    })
+    console.log(result);
+  }
+  
+//   updateUser("test@gmail.com", {
+//     firstName: "newfirstname",
+//     lastName: "newlastname"
+//   });
